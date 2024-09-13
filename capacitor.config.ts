4582ce607a482fa908a90bb70f99ef8ea6780ff4
eddiesidgo <124094058+eddiesidgo@users.noreply.github.com>
2024-09-12 21:48:29 -0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'medisphere-mobile-react-front',
+  webDir: 'dist'
+};
+
+export default config;
